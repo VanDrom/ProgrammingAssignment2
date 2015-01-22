672cb26dc2e5f4add659f7b46171a2b0ce735786
VanDrom <eddy_van_drom@hotmail.com>
2015-01-22 22:03:49 +0900

    non-stopping test

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,7 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function (I will try)
+## Write a short comment describing this function (I will try however)
 
 makeCacheMatrix <- function(x = matrix()) {
 
